de7280da910455fc73553125ae5207c07f59d832
Liubov <liubov.zhalba@yandex.ru>
2023-02-05 13:42:20 +1000

    создали файл

---
+++ Trening/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+void PrintSq(int n);
+{
+    int i = 1;
+    while(i <= n);
+    {
+        Console.Write(i + " ");
+        i++;   
+    }
+}
+Console.WriteLine("Input a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintSq(n);
